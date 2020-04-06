eac8216a1a5b33e78e8dc9494a67e6a9d2f0f6d2
root <root@localhost.localdomain>
2020-04-06 16:08:09 +0530

    Commit-2: APRIL 6th 2020

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+    # above maps any URLs starting with rango/ to be handled by the rango application
 ]
