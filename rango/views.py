8f7787247a726dbe772337cf35fd15db16bdf4ac
root <root@localhost.localdomain>
2020-04-06 16:29:49 +0530

    KAGE--Adding second commit to Branch_Exercise_3 <COMPLETED EXERCISE_3>  --->  APRIL 6th 2020

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<app_name> says hey there partner !")
+    return HttpResponse("<app_name> says hey there partner !<br/> <a href='/rango/about/'>About</a>")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.<br/> <a href='/rango/'>Index</a>")
