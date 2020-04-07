03ea3c80ee4efa95891d657746d185d63e13572a
root <root@localhost.localdomain>
2020-04-07 15:22:55 +0530

    CHAPTER-4 Completed APRIL 7th 2020

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+"""
+def index(request):
+    return HttpResponse("<app_name> says hey there partner !")
+"""
 
 
 def index(request):
-    return HttpResponse("<app_name> says hey there partner !")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return HttpResponse("This is about function in views.py file inside the app...")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,10 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     # above maps any URLs starting with rango/ to be handled by the rango application
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
