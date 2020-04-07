1aefb499b188b0d9b5833d2dd1ce66b04faf9862
root <root@localhost.localdomain>
2020-04-07 18:01:59 +0530

    CHAPTER-4 Exercise Completed APRIL 7th 2020

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<app_name> says hey there partner !<br/> <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<br/> <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rango import views
 
 urlpatterns = [
@@ -23,4 +26,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     # above maps any URLs starting with rango/ to be handled by the rango application
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
