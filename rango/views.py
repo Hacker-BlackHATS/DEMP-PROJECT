c69fcef682c159babaf797a0cf98da6ceba20369
root <root@localhost.localdomain>
2020-04-12 13:04:16 +0530

    CHAPTER-6 Little_Fatel Error APRIL 12th 2020

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    cate = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': cate}
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
